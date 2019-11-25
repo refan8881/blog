a2c2f004c2d23cf218863b979fd03cccd36af9a3
refan8881 <efan7151@gmail.com>
2019-11-25 11:44:50 +0700

    blog templete

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+  <!-- Head -->
+  <?php include('../layouts/includes/head.php')?>
+  <!-- end head -->
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    <!-- navbar -->
+  <?php include('../layouts/includes/navbar.php');?>
+    <!-- end navbar -->
+    <div class="app-body">
+      <!-- sidebar -->
+    <?php include('../layouts/includes/sidebar.php')?>
+      <!-- end sidebar -->
+     
+      <!-- Main -->
+      <main class="main">
+      </main>
+      <!-- End -->
+    </div>
+    <!-- footer -->
+    <?php include('../layouts/includes/footer.php')?>
+    <!-- end footer -->
+    <!-- CoreUI and necessary plugins-->
+    <!-- script -->
+      <?php include('../layouts/includes/script.php')?>
+    <!-- end script -->
+  </body>
+</html>
